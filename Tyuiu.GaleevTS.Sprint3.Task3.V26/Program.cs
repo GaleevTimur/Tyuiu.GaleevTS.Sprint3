94efe60caadd581c4ca61e05d79e18a863e16175
GaleevTimur <timur.galeev.05@yandex.ru>
2023-11-03 23:23:52 +0500

    Task3

---
+++ Tyuiu.GaleevTS.Sprint3.Task3.V26/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GaleevTS.Sprint3.Task3.V26.Lib;
+
+namespace Tyuiu.GaleevTS.Sprint3.Task3.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт №3 | Выполнил: Галеев Т. С. | ИИПб-23-3";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                *");
+            Console.WriteLine("* Тема: Оператор цикла foreach                                             *");
+            Console.WriteLine("* Задание №3                                                               *");
+            Console.WriteLine("* Вариант #26                                                              *");
+            Console.WriteLine("* Выполнил: Галеев Тимур Серикович | ИИПб-23-3                             *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                          *");
+            Console.WriteLine("****************************************************************************");
+            string value = "have a nice time";
+            char chr = 'e';
+            Console.WriteLine("Исходная строка = " + value);
+            Console.WriteLine("Искомый символ = " + chr);
+
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("Количество символов = " + ds.GetCharCount(value, chr));
+            Console.ReadKey();
+        }
+    }
+}
