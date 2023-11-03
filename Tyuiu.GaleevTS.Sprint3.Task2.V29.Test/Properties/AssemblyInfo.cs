8e3ab3f1979d22230e38911e0e6353f31820f555
GaleevTimur <timur.galeev.05@yandex.ru>
2023-11-03 23:13:14 +0500

    Task2

---
+++ Tyuiu.GaleevTS.Sprint3.Task2.V29.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GaleevTS.Sprint3.Task2.V29.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GaleevTS.Sprint3.Task2.V29.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("976a3796-f5c3-4f90-8f95-119ded8122df")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GaleevTS.Sprint3.Task2.V29/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GaleevTS.Sprint3.Task2.V29.Lib;
+
+namespace Tyuiu.GaleevTS.Sprint3.Task2.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт №3 | Выполнил: Галеев Т. С. | ИИПб-23-3";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                *");
+            Console.WriteLine("* Тема: Оператор цикла do-while                                            *");
+            Console.WriteLine("* Задание №2                                                               *");
+            Console.WriteLine("* Вариант #29                                                              *");
+            Console.WriteLine("* Выполнил: Галеев Тимур Серикович | ИИПб-23-3                             *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                          *");
+            Console.WriteLine("****************************************************************************");
+            int startValue = 1;
+            int stopValue = 18;
+            double value = 0.5;
+            Console.WriteLine("Переменная a = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
